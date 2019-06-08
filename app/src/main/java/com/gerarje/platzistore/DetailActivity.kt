eab12de180e62520f02a8be2344d50c3121e2fe7
Germán Arevalo <germanarevalojerez@gmail.com>
2019-06-08 12:29:44 -0500

    Transition to item detail

--- app/src/main/java/com/gerarje/platzistore/DetailActivity.kt
+++ app/src/main/java/com/gerarje/platzistore/DetailActivity.kt
@@ -3,6 +3,7 @@ package com.gerarje.platzistore
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_detail.*
+import kotlinx.android.synthetic.main.content_description.*
 
 class DetailActivity : AppCompatActivity() {
 
@@ -10,10 +11,10 @@ class DetailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
 
-        intent?.let {
-            val textFromMain:String = it.extras
-                .getString("text")
-            txtDetail.text = textFromMain
+        intent?.extras?.let {
+            txtDetailTitulo.text = it.getString("title")
+            txtDetailDesc.text = it.getString("desc")
+            txtDetailPrice.text = "$ ${String.format("%.2f", it.getDouble("price"))}"
         }
     }
 }
